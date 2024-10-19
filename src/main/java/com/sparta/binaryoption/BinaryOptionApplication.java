8e138e59e0b45d14d4b44f89372e810d5cf58780
jhwook <jhwooook@naver.com>
2024-10-19 22:33:37 +0900

    Initial commit

---
+++ src/main/java/com/sparta/binaryoption/BinaryOptionApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.binaryoption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaryOptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinaryOptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/binaryoption/BinaryOptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.binaryoption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinaryOptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
